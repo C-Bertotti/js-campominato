832bf82814dc90d0a9139e88cb8bed66a305fbed
Caterina Bertotti <bertotti4997@gmail.com>
2021-03-11 07:38:32 +0100

    corretto primo ciclo while

--- js/script.js
+++ js/script.js
@@ -46,14 +46,16 @@ function inArray(array, elemento) {
 //Genero 16 numeri casuali tra 1 e 100
 var bomba;
 var bombe = [];
-var doppiaBomba = inArray(bombe, bomba);
 
 //Fino a quando il numero non è incluso nell'array e la lunghezza dell'array è minore di 16 allora...
-while ( doppiaBomba = false  || bombe.length < 16 )  {
+while ( bombe.length < 16 )  {
     //...genero un numero
     bomba = getRndInteger(1, 100);
-    //Lo inserisco nell'array
+    //verifico che non sia nell'array
+    if ( inArray(bombe, bomba ) == false) {
+         //Lo inserisco nell'array
     bombe.push(bomba);
+    }
 }
 
 //Chiedo all'utente di inserire un numero compreso da 1 e 100
@@ -77,51 +79,3 @@ if (bombaEsplosa == true) {
 } else {
     console.log('Bravo, hai vinto!');
 }
-
-
-// controllo se è nell array, se si richiedo
-// if ( resultInArrayUser == true ) { 
-//     while ( resultInArrayUser == true ) {
-//         userNumber = parseInt(prompt('Hai già inserito questo numero, scegline un altro'));
-//     }
-// } else if ( UserNmIsInRndArray == true ) {
-//     console.log('Hai pestato sulla bomba!');
-//     if ( userNumbers.length <= 5 ) {
-//         console.log('La fortuna non ti asssite, sei riuscito a fare solamente ' + userNumbers.length + ' passi prima di beccarla!');
-//     } else if ( userNumbers.length > 5 && userNumbers.length <= 15 ) {
-//         console.log('Sei riuscito a trascinarti più in là della tua ombra di ' + userNumbers.length + ' passi, buono!');
-//     } else if ( userNumbers.length > 15 && userNumbers.length <= 40 ) {
-//         console.log('Hai fatto ' + userNumbers.length + ' passi, e sei riuscito a portarti a metà della terra di nessuno, mancava poco!');
-//     } else if ( userNumbers.length > 40 && userNumbers.length <= 100 ) {
-//         console.log('Ci sei quasiii!!! Con i tuoi ' + userNumbers.length + ' passi, sei riuscito a portarti quasi sul confine!');
-//     }
-// } else {
-//     userNumbers.push(userNumber);
-//     console.log(userNumbers);
-// }  
-
-
-// farei un ciclo while con condizione (UserNmIsInRndArray == false || userNumbers <= 84), ovvero fintanto che il numero non è presente nell'array dei random o raggiunge la max lunghezza di 84 elementi
-// while ( UserNmIsInRndArray == false || userNumbers <= 84 )  {
-//     while ( isNaN(userNumber) || userNumber < 1 || userNumber > 100 ) {
-//         userNumber = parseInt(prompt('Inserisci un numero compreso tra 1 e 100')); 
-//         while ( resultInArrayUser == true ) { 
-//             userNumber = parseInt(prompt('Hai già inserito questo numero, scegline un altro'));
-//         }
-//     } 
-//     userNumbers.push(userNumber);
-// } 
-
-// if ( UserNmIsInRndArray == true ) {
-    
-//     console.log('Hai pestato sulla bomba!');
-//     if ( userNumbers.length <= 5 ) {
-//         console.log('La fortuna non ti asssite, sei riuscito a fare solamente ' + userNumbers.length + ' passi prima di beccarla!');
-//     } else if ( userNumbers.length > 5 && userNumbers.length <= 15 ) {
-//         console.log('Sei riuscito a trascinarti più in là della tua ombra di ' + userNumbers.length + ' passi, buono!');
-//     } else if ( userNumbers.length > 15 && userNumbers.length <= 40 ) {
-//         console.log('Hai fatto ' + userNumbers.length + ' passi, e sei riuscito a portarti a metà della terra di nessuno, mancava poco!');
-//     } else if ( userNumbers.length > 40 && userNumbers.length <= 100 ) {
-//         console.log('Ci sei quasiii!!! Con i tuoi ' + userNumbers.length + ' passi, sei riuscito a portarti quasi sul confine!');
-//     }
-// }
