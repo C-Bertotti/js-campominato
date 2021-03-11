66dcfb90bd2c372f945de586750c5247982b5a5b
Caterina Bertotti <bertotti4997@gmail.com>
2021-03-11 09:13:23 +0100

    veste grafica

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 /* Consegna
 V Il computer deve generare 16 numeri casuali tra 1 e 100.
-V I numeri non possono essere duplicati. V
+V I numeri non possono essere duplicati. 
 V In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
 V L’utente non può inserire più volte lo stesso numero. 
-Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
-La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
-Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
-BONUS: (da fare solo se funziona tutto il resto)
+V Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
+V La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
+V Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
+V BONUS: (da fare solo se funziona tutto il resto)
 all’inizio il software richiede anche una difficoltà all’utente che cambia il range di numeri casuali:
 con difficoltà 0 => tra 1 e 100
 con difficoltà 1 => tra 1 e 80
