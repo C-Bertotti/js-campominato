c782e5d4b3d13fe51990b773253ff60ef3a9553d
Caterina Bertotti <bertotti4997@gmail.com>
2021-03-11 07:57:19 +0100

    aggiunto evento al click del botton che fa partire il gioco

--- js/script.js
+++ js/script.js
@@ -43,62 +43,71 @@ function inArray(array, elemento) {
     return false;
 }
 
-// Chiedo all'utente il grado di difficoltà
-do {
-    var level = parseInt(prompt('Scegli un livello di difficoltà tra 0, 1 o 2.'));
-} while ( isNaN(level) || level < 1 || level > 2 );
+// Creo le variabili 
+var gioca = document.getElementById('gioca__btn');
 
+// Creo l'evento al click del bottone 
+gioca.addEventListener('click',
+    function () {
+        // Chiedo all'utente il grado di difficoltà
+        do {
+            var level = parseInt(prompt('Scegli un livello di difficoltà tra 0, 1 o 2.'));
+        } while ( isNaN(level) || level < 0 || level > 2 );
 
-var rangeMax;
+        var rangeMax;
 
-switch (level) {
-    case 0:
-        rangeMax = 100;
-    break;
+        switch (level) {
+            case 0:
+                rangeMax = 100;
+            break;
 
-    case 1:
-        rangeMax = 80;
-    break;
+            case 1:
+                rangeMax = 80;
+            break;
 
-    case 2:
-        rangeMax = 50;
-    break;
-}
+            case 2:
+                rangeMax = 50;
+            break;
+        }
 
-//Genero 16 numeri casuali tra 1 e 100
-var bomba;
-var bombe = [];
+        //Genero 16 numeri casuali tra 1 e 100
+        var bomba;
+        var bombe = [];
 
-//Fino a quando il numero non è incluso nell'array e la lunghezza dell'array è minore di 16 allora...
-while ( bombe.length < 16 )  {
-    //...genero un numero
-    bomba = getRndInteger(1, rangeMax);
-    //verifico che non sia nell'array
-    if ( inArray(bombe, bomba ) == false) {
-         //Lo inserisco nell'array
-    bombe.push(bomba);
+        //Fino a quando il numero non è incluso nell'array e la lunghezza dell'array è minore di 16 allora...
+        while ( bombe.length < 16 )  {
+            //...genero un numero
+            bomba = getRndInteger(1, rangeMax);
+            //verifico che non sia nell'array
+            if ( inArray(bombe, bomba ) == false) {
+                //Lo inserisco nell'array
+            bombe.push(bomba);
+            }
+        }
+
+        //Chiedo all'utente di inserire un numero compreso da 1 e maxRange
+        var numeriUtente = [];
+        var bombaEsplosa = false;
+        var possibility = rangeMax - 16;
+
+        while ( numeriUtente.length < possibility && bombaEsplosa == false ) {
+            var numero = parseInt(prompt('inserisci un numero tra 1 e ' + rangeMax));
+
+            if ( isNaN(numero) || numero < 1 || numero > rangeMax ) {
+                alert('Non hai inserito un numero valido, riprova.');
+            } else if ( bombe.includes(numero) == true ) {
+                bombaEsplosa = true;
+            } else if (numeriUtente.includes(numero) == false) {
+                numeriUtente.push(numero);
+            }
+        }
+
+        if (bombaEsplosa == true) {
+            console.log('Hai preso la bomba dopo aver fatto ' + numeriUtente.length + ' passi.');
+        } else {
+            console.log('Bravo, hai vinto!');
+        }
     }
-}
+   
+);
 
-//Chiedo all'utente di inserire un numero compreso da 1 e maxRange
-var numeriUtente = [];
-var bombaEsplosa = false;
-var possibility = rangeMax - 16;
-
-while ( numeriUtente.length < possibility && bombaEsplosa == false ) {
-    var numero = parseInt(prompt('inserisci un numero tra 1 e ' + rangeMax));
-
-    if ( isNaN(numero) || numero < 1 || numero > rangeMax ) {
-        alert('Non hai inserito un numero valido, riprova.');
-    } else if ( bombe.includes(numero) == true ) {
-        bombaEsplosa = true;
-    } else if (numeriUtente.includes(numero) == false) {
-        numeriUtente.push(numero);
-    }
-}
-
-if (bombaEsplosa == true) {
-    console.log('Hai preso la bomba dopo aver fatto ' + numeriUtente.length + ' passi.');
-} else {
-    console.log('Bravo, hai vinto!');
-}
